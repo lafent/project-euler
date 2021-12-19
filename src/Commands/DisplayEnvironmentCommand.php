9ed23c82c890728e7fc7e9d13b3db910ab192090
Lee Fent <lee.fent@gmail.com>
2021-12-18 21:34:25 -0600

    Start of Progress.

--- src/Commands/DisplayEnvironmentCommand.php
+++ src/Commands/DisplayEnvironmentCommand.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace ProjectEurler\Commands;
+namespace ProjectEuler\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
